3543c40bf92be7aae59fa86981a91e7926a15537
Henry <henry983615@gmail.com>
2023-12-30 15:13:17 +1300

    images list via array

--- simple-event-gallery-fe/src/app/gallery/gallery.component.ts
+++ simple-event-gallery-fe/src/app/gallery/gallery.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {GalleryItemInterface} from "../interfaces/galleryinterfaces";
+import {GalleryItemComponentComponent} from "../gallery-item-component/gallery-item-component.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [
+    GalleryItemComponentComponent,
+    NgForOf
+  ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 
 export class GalleryComponent {
-
   readonly baseUrl = '../assets/gallery-data/';
-  galleryData: GalleryItemInterface[] = [];
+  galleryData: GalleryItemInterface[] = [
+    {
+      path: `${this.baseUrl}1.jpeg`,
+      alt: "image alt text"
+    },
+    {
+      path: `${this.baseUrl}2.jpeg`,
+      alt: "image alt text"
+    },
+    {
+      path: `${this.baseUrl}3.jpeg`,
+      alt: "image alt text"
+    },
+    {
+      path: `${this.baseUrl}4.jpeg`,
+      alt: "image alt text"
+    }
+  ];
 
   examplePhoto: GalleryItemInterface = {
     path: "1.jpeg",
-    alt: "image"
+    alt: "manual image"
   };
 
   constructor() {}
